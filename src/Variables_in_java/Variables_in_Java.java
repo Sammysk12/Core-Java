779fda27f1492dae490bd76130b323841b887fb8
Sammysk12 <samarthkalshetti43@gmail.com>
2025-01-03 20:54:12 +0530

    intial commit

---
+++ src/Variables_in_java/Variables_in_Java.java
@@ -0,0 +1,11 @@
+package Variables_in_java;
+
+public class Variables_in_Java {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+
+    } //end main
+}// end class
+// This Entire code is called as boiler plate code means basic code for start java code
